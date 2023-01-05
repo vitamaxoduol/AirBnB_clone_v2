35e74c0f03af7f9d42a6f4d63da12736d294cdfe
Uwimana Lowami <uwilowami64@outlook.com>
2023-01-05 11:00:29 -0800

    Solve tasks #3 and #4

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- Create a database, a user and permissions
+
+CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT ALL ON hbnh_dev_db.* TO "hbnb_dev"@"localhost";
+GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
